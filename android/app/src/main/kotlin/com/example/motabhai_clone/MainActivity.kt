279ff6c2c9d838dfcaf74c4ba569cf3e25ce5384
vikil143 <vikil.lakkavatri25@gmail.com>
2021-03-08 20:53:25 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/motabhai_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.motabhai_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0xFF2C3D63);
+Color darkGreen = Color(0xFF008080);
+Color screen1Light = Color(0xFFADDCCA);
+Color whiteColor = Color(0xFFFFFFFF);
+Color blackColor = Color(0xFF000000);
+Color lightGrey = Color(0xffdddddd);
